97ceb2fb109b4f4458f67a677d0bd69a38efc45f
Madan Sapkota <contact@madansapkota.com>
2024-07-31 23:42:27 +0545

    Add method to create event

--- src/GoogleCalendar.php
+++ src/GoogleCalendar.php
@@ -6,6 +6,7 @@ namespace Intuji\Events;
 
 use Google\Service\Exception;
 use Google_Service_Calendar;
+use Google_Service_Calendar_Event;
 
 final class GoogleCalendar
 {
@@ -31,5 +32,25 @@ final class GoogleCalendar
         ]);
 
         return $results->getItems();
+    }
+
+    /**
+     * Create an event in the Google Calendar
+     * @throws Exception
+     */
+    public function createEvent(array $event, $calendarId = 'primary'): bool
+    {
+        // Get the Google client
+        $client = (new GoogleAuth())->getClient();
+        $client->setAccessToken($_SESSION['access_token']);
+
+        // Get the Google Calendar service
+        $service = new Google_Service_Calendar($client);
+
+        // Create the event
+        $event = new Google_Service_Calendar_Event($event);
+        $res = $service->events->insert($calendarId, $event);
+
+        return $res->status === 'confirmed';
     }
 }
