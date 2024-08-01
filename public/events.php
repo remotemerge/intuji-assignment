2839d0ae0d3032589bebc6668e5f22769fe6e123
Madan Sapkota <contact@madansapkota.com>
2024-08-01 14:20:48 +0545

    Fetch events from primary calendar

---
+++ public/events.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Intuji\Events\GoogleAuth;
+use Intuji\Events\GoogleCalendar;
+
+// Bootstrap the application
+require dirname(__DIR__) . '/bootstrap/app.php';
+
+// Get the Google client
+$client = (new GoogleAuth())->getClient();
+
+// Init events
+$events = [];
+
+// Access token is available
+if (isset($_SESSION['access_token'])) {
+    $calendarId = 'primary';
+    $calendar = new GoogleCalendar();
+    $events = $calendar->getEvents($calendarId);
+}
