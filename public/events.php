dc381e50811f1b829b34e1cec4e329f2494a751d
Madan Sapkota <contact@madansapkota.com>
2024-08-01 14:42:01 +0545

    Unset session if token expired

--- public/events.php
+++ public/events.php
@@ -11,11 +11,16 @@ require dirname(__DIR__) . '/bootstrap/app.php';
 // Get the Google client
 $client = (new GoogleAuth())->getClient();
 
+// Access token is expired
+if ($client->isAccessTokenExpired()) {
+    unset($_SESSION['access_token']);
+}
+
 // Init events
 $events = [];
 
 // Access token is available
-if (isset($_SESSION['access_token']) && !$client->isAccessTokenExpired()) {
+if (isset($_SESSION['access_token'])) {
     $calendarId = 'primary';
     $calendar = new GoogleCalendar();
     $events = $calendar->getEvents($calendarId);
--- public/login.php
+++ public/login.php
@@ -8,8 +8,13 @@ require dirname(__DIR__) . '/bootstrap/app.php';
 // Get the Google client
 $client = (new Intuji\Events\GoogleAuth())->getClient();
 
+// Access token is expired
+if ($client->isAccessTokenExpired()) {
+    unset($_SESSION['access_token']);
+}
+
 // Access token is not available
-if (!isset($_SESSION['access_token']) || $client->isAccessTokenExpired()) {
+if (!isset($_SESSION['access_token'])) {
     //  Redirect to login page
     $authUrl = (new Intuji\Events\GoogleAuth())->getAuthUrl();
     header('Location: ' . $authUrl);
