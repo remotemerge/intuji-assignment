f7f969cae27f93bdfd880c5acb80499746343980
Madan Sapkota <contact@madansapkota.com>
2024-08-01 10:51:16 +0545

    Add login feature

---
+++ public/login.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+// Bootstrap the application
+require dirname(__DIR__) . '/bootstrap/app.php';
+
+// Start the session
+session_start();
+
+// Get the Google client
+$client = (new Intuji\Events\GoogleAuth())->getClient();
+
+// Access token is available
+if (isset($_SESSION['access_token'])) {
+    // Set the access token
+    $client->setAccessToken($_SESSION['access_token']);
+} else {
+    //  Redirect to login page
+    $authUrl = (new Intuji\Events\GoogleAuth())->getAuthUrl();
+    header('Location: ' . $authUrl);
+}
+
+// Check if access token is expired
+if ($client->isAccessTokenExpired()) {
+    // Refresh the access token
+    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+    $_SESSION['access_token'] = $client->getAccessToken();
+}
+
+// Redirect to the home page
+header('Location: /');
